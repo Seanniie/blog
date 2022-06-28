f8f527be01a5a42ae750e545aa3981afef88261e
Seannie <shlim4915@gmail.com>
2022-06-28 22:52:24 +0800

    간단한 블로그 폼

---
+++ src/App.js
@@ -0,0 +1,63 @@
+/* eslint-disable*/
+
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  
+  let post = '강남 우동 맛집';
+  let [글제목,titleChange] = useState(['남자 코트 추천','강남 우동 맛집','파이썬독학']);
+  let [따봉, 따봉변경] = useState(0);
+  return (
+
+    <div className="App">
+      <div className="black-nav">
+        <h4>ReactBlog</h4>
+      </div>
+
+      <button onClick={() => {
+
+        let copy = [...글제목];
+        copy[0] = '여자코트 추천';
+        titleChange(copy);
+
+      }}>글수정</button>
+
+      <div className='list'>
+        <h4>{글제목[0]} <span onClick={()=>{따봉변경(따봉+1)}}>👍</span> {따봉} </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{글제목[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+
+      
+      <Modal/>
+      
+    </div>
+    
+  );
+
+};
+
+function Modal(){
+  return (
+    <>
+      <div className='modal'>
+          <h4>title</h4>
+          <p>date</p>
+          <p>detail</p>
+      </div>
+      <div></div>
+    </>
+  )
+
+};
+
+export default App;
